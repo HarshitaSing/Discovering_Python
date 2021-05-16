dea6d94499f29f202c4ded46f16b7d17b09df611
Harshita Singh <30619470+HarshitaSing@users.noreply.github.com>
2021-05-16 19:55:48 +0530

    Update list.py

--- Basic_Data_Types/Lists/list.py
+++ Basic_Data_Types/Lists/list.py
@@ -2,11 +2,20 @@ if __name__ == '__main__':
     N = int(input())
     L = []
     for _ in range(N):
+        #splitting the input using default parameter - space like -> [insert, 5, 10]
         s = input().split()
+        #storing the first string (e.g. "insert")
         cmd = s[0]
+        #stroring all rest of the characters
         args = s[1:]
         if cmd !="print":
             cmd += "("+ ",".join(args) +")"
             eval("L."+cmd)
         else:
             print (L)
+
+    '''
+    Tried this code in Python 3.x using input() function
+    Same can be achieved using raw_input() function in Python2.x
+    '''
+    
